bdf9571db8bd5e0c7ef9e175dba9ed0caf9b178c
AnuShafi <120824041+AnuShafi@users.noreply.github.com>
2022-12-17 16:53:00 -0600

    Update ServiceException.java

--- src/main/java/com/example/customer_reward/utils/ServiceException.java
+++ src/main/java/com/example/customer_reward/utils/ServiceException.java
@@ -6,11 +6,61 @@ public class ServiceException extends RuntimeException {
 	
 	private static final long serialVersionUID = 1L;
 	
+	String message;
+	
+	String error;
+	
 	StatusCode statusCode;
+	
+	public ServiceException() {
+		
+	}
 	
 	public ServiceException(StatusCode code) {
 	this.statusCode = code;	
 	}
+	
+	
+
+	public String getMessage() {
+		return message;
+	}
+
+
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+
+
+	public String getError() {
+		return error;
+	}
+
+
+
+	public void setError(String error) {
+		this.error = error;
+	}
+
+
+
+	public StatusCode getStatusCode() {
+		return statusCode;
+	}
+
+
+
+	public void setStatusCode(StatusCode statusCode) {
+		this.statusCode = statusCode;
+	}
+	 
+	@Override
+	    public synchronized Throwable fillInStackTrace() {
+	        return this;
+	    }
+
 
 	public enum StatusCode{
 		
